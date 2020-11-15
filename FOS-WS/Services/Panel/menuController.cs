9fa6e65e351da08cfea53d85957d4d12335ca634
hediyeh naderi <naderi.h.72@gmail.com>
2020-11-15 16:59:33 +0330

    create contoller for panel

---
+++ FOS-WS/Services/Panel/menuController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FOS_WS.Models;
+
+namespace FOS_WS.Services.Panel
+{
+    public class menuController : ApiController
+    {
+        private FOSWSDB db = new FOSWSDB();
+
+        // GET: api/menu
+        public IQueryable<Food> GetFoods()
+        {
+            return db.Foods;
+        }
+
+        // GET: api/menu/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult GetFood(int id)
+        {
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(food);
+        }
+
+        // PUT: api/menu/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFood(int id, Food food)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != food.FID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(food).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/menu
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult PostFood(Food food)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Foods.Add(food);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = food.FID }, food);
+        }
+
+        // DELETE: api/menu/5
+        [ResponseType(typeof(Food))]
+        public IHttpActionResult DeleteFood(int id)
+        {
+            Food food = db.Foods.Find(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            db.Foods.Remove(food);
+            db.SaveChanges();
+
+            return Ok(food);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FoodExists(int id)
+        {
+            return db.Foods.Count(e => e.FID == id) > 0;
+        }
+    }
+}
